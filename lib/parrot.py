c45159698656bd2d03a2968ad0dcd079d87ad797
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-05-23 12:34:08 -0400

    parrot lab complete

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,15 @@
-def parrot():
-    pass
+
+#prints the argument passed to it
+#returns the argument passed to it
+# def parrot(arg):
+#     print(arg)
+#     return(arg)
+
+#returns the argument passed to it
+# def parrot(arg):
+#     return(arg)
+
+#prints "Squawk!" if no argument is passed.
+def parrot(arg="Squawk!"):
+    print(arg)
+    return arg
